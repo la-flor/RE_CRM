102ad667d1fd9ca0e4c8ab7a0ec3f794156f5386
lafBot <64456835+lafBot@users.noreply.github.com>
2021-03-08 19:37:59 -0700

    Created navbar component

---
+++ frontend/src/Navbar.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Navbar.css";
+
+const Navbar = () => {
+    return (
+        <div className="Navbar">
+            <span>
+                How It Works
+            </span>
+            <span>
+                FAQ
+            </span>
+        </div>
+    )
+}
+
+export default Navbar;
