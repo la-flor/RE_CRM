a6b54c881577d6b86792f946dd639e26fd7cc1aa
lafBot <64456835+lafBot@users.noreply.github.com>
2021-03-21 09:11:06 -0600

    Created initial lead form without functionality

--- frontend/src/components/NewLeadForm.js
+++ frontend/src/components/NewLeadForm.js
@@ -33,40 +33,104 @@ const NewLeadForm = () => {
     }
 
     return (
-        <div className="NewLeadForm p-5 mt-4">
+        <div className="NewLeadForm container p-xl-5 mt-4">
             <form className="form-group">
-                <label htmlFor="title">Title: </label>
-                <input
-                    name="title"
-                    type="text"
-                    value={formData.title}
-                    onChange={handleChange}
-                    className="form-control"
-                />
-                <br/>
+                <div className="NewLeadForm-row row">
+                    <div className="col-lg px-lg-5 pb-5 pb-lg-0">
+                        <h3 className="pb-lg-4 pb-3">Contact Information</h3>
+                        <label htmlFor="first">First: </label>
+                        <input
+                            name="first"
+                            type="text"
+                            value={formData.first}
+                            onChange={handleChange}
+                            className="form-control"
+                        />
+                        <br />
 
-                <label htmlFor="description">Description: </label>
-                <input
-                    name="description"
-                    type="text"
-                    value={formData.description}
-                    onChange={handleChange}
-                    className="form-control"
-                />
-                <br/>
+                        <label htmlFor="last">Last: </label>
+                        <input
+                            name="last"
+                            type="text"
+                            value={formData.last}
+                            onChange={handleChange}
+                            className="form-control"
+                        />
+                        <br />
 
-                <label htmlFor="body">Body: </label>
-                <input
-                    name="body"
-                    type="text"
-                    value={formData.body}
-                    onChange={handleChange}
-                    className="form-control"
-                />
-                <br/>
-                <button type="submit" onClick={handleSubmit} className="NewLeadForm-saveBtn btn btn-success">Save</button>
-                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                <button onClick={goHome} className="NewLeadForm-cancelBtn btn btn-warning">Cancel</button>
+                        <label htmlFor="email">Email: </label>
+                        <input
+                            name="email"
+                            type="text"
+                            value={formData.email}
+                            onChange={handleChange}
+                            className="form-control"
+                        />
+                        <br />
+
+                        <label htmlFor="phone">Phone: </label>
+                        <input
+                            name="phone"
+                            type="text"
+                            value={formData.phone}
+                            onChange={handleChange}
+                            className="form-control"
+                        />
+                        <br />
+                    </div>
+
+
+                    <div className="col-lg px-lg-5 pb-3 pb-lg-0">
+                        <h3 className="pb-lg-4 pb-3">Property Address</h3>
+                        <label htmlFor="address">Address: </label>
+                        <input
+                            name="address"
+                            type="text"
+                            value={formData.address}
+                            onChange={handleChange}
+                            className="form-control"
+                        />
+                        <br />
+
+                        <label htmlFor="city">City: </label>
+                        <input
+                            name="city"
+                            type="text"
+                            value={formData.city}
+                            onChange={handleChange}
+                            className="form-control"
+                        />
+                        <br />
+
+                        <label htmlFor="state">State: </label>
+                        <input
+                            name="state"
+                            type="text"
+                            value={formData.state}
+                            onChange={handleChange}
+                            className="form-control"
+                        />
+                        <br />
+
+                        <label htmlFor="zipcode">Zip Code: </label>
+                        <input
+                            name="zipcode"
+                            type="text"
+                            value={formData.zipcode}
+                            onChange={handleChange}
+                            className="form-control"
+                        />
+                    </div>
+                </div>
+
+                <div className="NewLeadForm-row row">
+                    <div className="NewLeadForm-submit-container col-md px-lg-5 pt-3">
+                        <button type="submit" onClick={handleSubmit} className="NewLeadForm-submit-btn btn">Submit</button>
+                        <p className="pt-3">
+                            By submitting this form you are agreeing to allow our team to review your property information and contact you.
+                        </p>
+                    </div>
+                </div>
             </form>
         </div>
     )
