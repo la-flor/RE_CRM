c0490aeaa4b66c4f6f16859cc4ed3f43cf2733c9
lafBot <64456835+lafBot@users.noreply.github.com>
2021-03-22 17:52:22 -0600

    Updated api url route and response

--- frontend/src/API/api.js
+++ frontend/src/API/api.js
@@ -4,10 +4,11 @@ const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:5000";
 
 async function submitOfferToAPI(data) {
     try {
-        const resp = await axios.post(`${BASE_URL}/newlead`, data);
+        const resp = await axios.post(`${BASE_URL}/new_lead`, data);
         if (!resp.data) {
             throw new Error("No response data recieved");
         }
+        console.log("Customer information succesfully submitted.")
     } catch(err) {
         console.error("There was an error submitting the form.", err);
     }
