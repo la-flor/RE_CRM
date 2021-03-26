e89201a70e179cfd3c6f5cdca01263e11ca1880b
lafBot <64456835+lafBot@users.noreply.github.com>
2021-03-26 13:48:17 -0600

    Added error handling for api calls

--- frontend/src/API/api.js
+++ frontend/src/API/api.js
@@ -4,13 +4,17 @@ const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:5000";
 
 async function submitOfferToAPI(data) {
     try {
-        const resp = await axios.post(`${BASE_URL}/new_lead`, data);
-        if (!resp.data) {
-            throw new Error("No response data recieved");
+        const resp = (await axios.post(`${BASE_URL}/new_lead`, data)).data;
+
+        if (resp.errors) {
+            throw (resp.errors)
         }
-        console.log("Customer information succesfully submitted.")
+
+        return resp;
     } catch(err) {
-        console.error("There was an error submitting the form.", err);
+        console.error("API Error:", err);
+        let message = err;
+        throw Array.isArray(message) ? message : [message];
     }
 }
 
